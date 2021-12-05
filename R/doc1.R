bf551163c7f37beef0486046ef700ab777b08325
gioneves <gioneves@alumni.usp.br>
2021-12-05 20:25:46 -0300

    listReduce

--- R/doc1.R
+++ R/doc1.R
@@ -2,7 +2,7 @@
 #'
 #' Compare and returns commom columns of the datasets within a list, faster and easy.
 #'
-#' @usage reduceList(mylist)
+#' @usage listReduce(mylist)
 #'
 #' @param mylist a list
 #'
@@ -15,6 +15,8 @@
 #' df_2 = data.frame(x = 7:8, y = 9:10, z = 11:12),
 #' df_3 = data.frame(x = 13:14, j = 15:16, z = 17:18)
 #' )
+#'
+#' listReduce(mylist = list1)
 #'
 #' @export
 
